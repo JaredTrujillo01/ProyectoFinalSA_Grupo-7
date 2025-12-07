141cabca64dcab5edeb25b4456ab1d7716660a9a
RobertOrtega <roberorteg017@gmail.com>
2025-12-07 00:10:41 -0600

    Agrego carpeta Panel de Control con HTML, CSS y JS

---
+++ frontend/panel-control/script.js
@@ -0,0 +1,6 @@
+
+const totalSucursales = 5;
+const totalCarros = 27;
+
+document.getElementById("suc-total").innerText = totalSucursales;
+document.getElementById("car-total").innerText = totalCarros;
